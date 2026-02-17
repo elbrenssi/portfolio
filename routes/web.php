6e4d7f224c8bb607a536be7fdf3c4b3157611a8c
elbrenssi <elbrenssi.reda@gmail.com>
2026-02-17 17:19:05 +0000

    ER

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\PortfolioController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', PortfolioController::class)->name('home');
-Route::get('/sitemap.xml', [PortfolioController::class, 'sitemap'])->name('sitemap');
-Route::get('/robots.txt', [PortfolioController::class, 'robots'])->name('robots');
